93a0af90a9e724f0e915150fccea6cec13479c57
Vylich <ilya.sidorenko.99@gmail.com>
2023-08-27 17:02:53 +0300

    .

--- source/js/modules/accordion.js
+++ source/js/modules/accordion.js
@@ -1,20 +1,26 @@
-const headers = document.querySelectorAll('.accordion__header');
+
+const items = document.querySelectorAll('.accordion__item');
 
 const showContentAccordion = () => {
-  headers.forEach((header) => {
-    header.insertAdjacentHTML('beforeEnd', '<button></button>');
-    header.addEventListener('click', () => {
-      header.blur();
-      const activeContent = document.querySelector('#' + header.dataset.acc);
-      if (activeContent.classList.contains('is-active')) {
-        activeContent.classList.remove('is-active');
-        header.classList.remove('is-active');
-        activeContent.style.maxHeight = 0;
-      } else {
-        header.classList.add('is-active');
-        activeContent.classList.add('is-active');
-        activeContent.style.maxHeight = activeContent.scrollHeight + 'px';
-      }
+  items.forEach((item) => {
+    const headers = item.querySelectorAll('.accordion__header');
+    headers.forEach((header) => {
+      header.insertAdjacentHTML('beforeEnd', '<button></button>');
+      header.addEventListener('click', () => {
+        header.blur();
+        const activeContent = document.querySelector('#' + header.dataset.acc);
+        if (activeContent.classList.contains('is-active')) {
+          activeContent.classList.remove('is-active');
+          header.classList.remove('is-active');
+          activeContent.style.maxHeight = 0;
+          item.classList.remove('is-active');
+        } else {
+          header.classList.add('is-active');
+          activeContent.classList.add('is-active');
+          item.classList.add('is-active');
+          activeContent.style.maxHeight = activeContent.scrollHeight + 'px';
+        }
+      });
     });
   });
 };
